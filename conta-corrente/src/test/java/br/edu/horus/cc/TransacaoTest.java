f28d282356215254097edec58d756bea40596772
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2016-03-09 21:07:35 -0300

    Testes para tranferencia

--- conta-corrente/src/test/java/br/edu/horus/cc/TransacaoTest.java
+++ conta-corrente/src/test/java/br/edu/horus/cc/TransacaoTest.java
@@ -1,6 +1,8 @@
 package br.edu.horus.cc;
 
 
+import static org.junit.Assert.fail;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,6 +45,30 @@ public class TransacaoTest {
 	public void testNaoDeixaSaldoNegativo(){
 		contas.put("2", 9.01);
 		transacao.sacar("2", 9.02);
+	}
+	
+	/*
+	 * Não deve transferir quando a conta da qual vai ser debitado esta sem saldo
+	 */
+	@Test(expected = SaldoNegativoException.class)
+	public void testNaoDeveTransaferirQuandoSemSaldo(){
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Não deve transferir quando a primeira conta não existir
+	 */
+	@Test
+	public void testNaoDeveTransaferirQuandoPrimeiraContaNaoExiste(){
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Não deve transferir quando a segunda conta não existir
+	 */
+	@Test(expected = SaldoNegativoException.class)
+	public void testNaoDeveTransaferirQuandoSegundaContaNaoExiste(){
+		fail("Not yet implemented");
 	}
 }
 
